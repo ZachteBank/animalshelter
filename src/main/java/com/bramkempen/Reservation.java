bd29da3a614253b3e3fcca4890abd7c3f296e892
bramk <bramkempen@gmail.com>
2018-03-06 12:58:17 +0100

    add first test

--- src/main/java/com/bramkempen/Reservation.java
+++ src/main/java/com/bramkempen/Reservation.java
@@ -4,12 +4,20 @@ import com.animals.Animal;
 import com.animals.Cat;
 import com.animals.Dog;
 import com.animals.Gender;
+import com.sun.javafx.UnmodifiableArrayList;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class Reservation {
 
     private ArrayList<Animal> animals = new ArrayList<>();
+
+    public List<Animal> getAnimals(){
+        return Collections.unmodifiableList(animals);
+    }
 
     public void newCat(String name, Gender gender, String badHabits){
         this.animals.add(new Cat(name, gender, badHabits));
---
+++ src/test/java/ReservationTest.java
@@ -0,0 +1,28 @@
+import com.animals.Gender;
+import com.bramkempen.Reservation;
+import junit.framework.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReservationTest {
+    private Reservation reservation;
+
+    @Before
+    public void TestInitialize(){
+        this.reservation = new Reservation();
+    }
+
+    @Test
+    public void TestNewCat(){
+        Assert.assertEquals(0, this.reservation.getAnimals().size());
+        this.reservation.newCat("Ms. Meow", Gender.Female, "Scratches couch");
+        Assert.assertEquals(1, this.reservation.getAnimals().size());
+    }
+
+    @Test
+    public void TestNewDog(){
+        Assert.assertEquals(0, this.reservation.getAnimals().size());
+        this.reservation.newDog("Sgt. Woof", Gender.Male);
+        Assert.assertEquals(1, this.reservation.getAnimals().size());
+    }
+}
